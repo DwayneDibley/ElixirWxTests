94a44c0db6c16c89257d92fcd1493b4caf65e409
DwayneDibley <roger.wenham@gmx.net>
2018-10-04 15:35:57 +0200

    update

---
+++ lib/wx_tests.ex
@@ -0,0 +1,34 @@
+defmodule WxTests do
+  use Mix.Task
+  @moduledoc """
+  Documentation for WxTests.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WxTests.hello()
+      :world
+
+  """
+
+  def main(a) do
+    IO.inspect("Hello world 1")
+  end
+
+  def start(a,b) do
+      IO.inspect("Hello world 2")
+      {:ok, self()}
+    end
+
+  def hello do
+    IO.inspect("Hello world 3")
+  end
+
+  def run(_) do
+    IO.inspect("Hello world 3")
+  end
+
+end
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule WxTests.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :wx_tests,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      #escript: escript(),
+      deps: deps()
+    ]
+  end
+
+  # Must be added.
+  def escript do
+    [main_module: WxTests]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {WxTests, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+     {:elixirwx, git: "https://github.com/DwayneDibley/ElixirWx.git"}
+    ]
+  end
+end
